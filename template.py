9bb3a8bd27e5c97eb5536417e292f0b531d32c1d
ZeroCipherX <saqibkhank805@gmail.com>
2026-03-26 23:04:10 +0500

    Project Structure

---
+++ template.py
@@ -0,0 +1,73 @@
+import os
+from pathlib import Path
+import logging
+
+# Logging setup to track file creation
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "text-insight-mlops-pipeline"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    # --- ENTITY (Data Classes) ---
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    # --- COMPONENTS (The Workers) ---
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_validation.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_evaluation.py",
+    # --- PIPELINE STAGES (The Orchestrators) ---
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py",
+    f"src/{project_name}/pipeline/stage_02_data_validation.py",
+    f"src/{project_name}/pipeline/stage_03_data_transformation.py",
+    f"src/{project_name}/pipeline/stage_04_model_trainer.py",
+    f"src/{project_name}/pipeline/stage_05_model_evaluation.py",
+    f"src/{project_name}/pipeline/prediction.py",
+    # --- ROOT CONFIGS & APP ---
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    # --- RESEARCH NOTEBOOKS ---
+    "research/01_data_ingestion.ipynb",
+    "research/02_data_validation.ipynb",
+    "research/03_data_transformation.ipynb",
+    "research/04_model_trainer.ipynb",
+    "research/05_model_evaluation.ipynb",
+    "research/trials.ipynb",
+    # --- TEMPLATES (UI) ---
+    "templates/index.html",
+    "templates/results.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # Create directories
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    # Create empty files (if they don't exist or are size 0)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
